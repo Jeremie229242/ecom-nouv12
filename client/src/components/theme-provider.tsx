1abf5fdaf063e9765fc67c46340055c1338df431
unknown <jeremie229242@gmail.com>
2026-08-06 09:30:00 +0100

    configuration projet-cli-lay-car

--- client/src/components/theme-provider.tsx
+++ client/src/components/theme-provider.tsx
@@ -67,7 +67,10 @@ export const useTheme = () => {
   const context = useContext(ThemeProviderContext)
 
   if (context === undefined)
+
     throw new Error("useTheme must be used within a ThemeProvider")
 
   return context
+
 }
+
